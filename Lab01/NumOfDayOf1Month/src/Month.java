78a601aa338b795ab850aecd6c5d4ebc20b7195c
khanhvecto <89617684+khanhvecto@users.noreply.github.com>
2023-04-15 19:30:04 +0700

    Ex 6.4

--- Lab01/NumOfDayOf1Month/src/Month.java
+++ Lab01/NumOfDayOf1Month/src/Month.java
@@ -16,11 +16,6 @@ public class Month {
 			}
 			year = (int) year_check;
 			
-			// Check if year is leap or not
-			if(year%4 != 0) leap = 0;
-			else if(year%100 == 0 && year%400 != 0) leap = 0;
-			else leap = 1;
-			
 			// Input month
 			System.out.println("Input month: ");
 			keyboard.nextLine();
@@ -32,80 +27,62 @@ public class Month {
 			case "Jan.":
 			case "Jan":
 			case "1":
+			case "March":
+			case "Mar.":
+			case "Mar":
+			case "3":
+			case "May":
+			case "5":
+			case "July":
+			case "Jul":
+			case "7":
+			case "August":
+			case "Aug.":
+			case "Aug":
+			case "8":
+			case "October":
+			case "Oct.":
+			case "Oct":
+			case "10":
+			case "December":
+			case "Dec.":
+			case "Dec":
+			case "12":
 				System.out.println("This month has 31 days.");
 				break;
-			case "January":
-			case "Jan.":
-			case "Jan":
-			case "1":
-				System.out.println("This month has 31 days.");
+			case "February":
+			case "Feb.":
+			case "Feb":
+			case "2":
+				// Check if year is leap or not
+				if(year%4 != 0) leap = 0;
+				else if(year%100 == 0 && year%400 != 0) leap = 0;
+				else leap = 1;
+				
+				if(leap == 1) System.out.println("This month has 29 days.");
+				else System.out.println("This month has 28 days.");
 				break;
-			case "January":
-			case "Jan.":
-			case "Jan":
-			case "1":
-				System.out.println("This month has 31 days.");
-				break;
-			case "January":
-			case "Jan.":
-			case "Jan":
-			case "1":
-				System.out.println("This month has 31 days.");
-				break;
-			case "January":
-			case "Jan.":
-			case "Jan":
-			case "1":
-				System.out.println("This month has 31 days.");
-				break;
-			case "January":
-			case "Jan.":
-			case "Jan":
-			case "1":
-				System.out.println("This month has 31 days.");
-				break;
-			case "January":
-			case "Jan.":
-			case "Jan":
-			case "1":
-				System.out.println("This month has 31 days.");
-				break;
-			case "January":
-			case "Jan.":
-			case "Jan":
-			case "1":
-				System.out.println("This month has 31 days.");
-				break;
-			case "January":
-			case "Jan.":
-			case "Jan":
-			case "1":
-				System.out.println("This month has 31 days.");
-				break;
-			case "January":
-			case "Jan.":
-			case "Jan":
-			case "1":
-				System.out.println("This month has 31 days.");
-				break;
-			case "January":
-			case "Jan.":
-			case "Jan":
-			case "1":
-				System.out.println("This month has 31 days.");
-				break;
-			case "January":
-			case "Jan.":
-			case "Jan":
-			case "1":
-				System.out.println("This month has 31 days.");
+			case "April":
+			case "Apr.":
+			case "Apr":
+			case "4":
+			case "June":
+			case "Jun":
+			case "6":
+			case "September":
+			case "Sept.":
+			case "Sep":
+			case "9":
+			case "November":
+			case "Nov.":
+			case "Nov":
+			case "11":
+				System.out.println("This month has 30 days.");
 				break;
 			default:
 				System.out.println("Unvalid month. Please input again.");
 				continue;
-				break;
 			}
-			
 			break;
 		}
 		while(true);
