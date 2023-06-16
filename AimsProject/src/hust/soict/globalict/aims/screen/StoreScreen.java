4efd0a06da31c9b9df819204a5be1ecd76b29f66
khanhvecto <89617684+khanhvecto@users.noreply.github.com>
2023-06-16 09:00:35 +0700

    init

---
+++ AimsProject/src/hust/soict/globalict/aims/screen/StoreScreen.java
@@ -0,0 +1,51 @@
+package hust.soict.globalict.aims.screen;
+import javax.swing.*;
+import hust.soict.globalict.aims.store.Store.*;
+import java.awt.*;
+public class StoreScreen extends JFrame{
+	private Store store;
+	
+	public StoreScreen() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	JPanel createNorth() {
+		JPanel north = new JPanel();
+		north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
+		north.add(createMenuBar());
+		north.add(createHeader());
+		return north;
+	}
+	
+	JMenuBar createMenuBar() {
+		JMenu menu = new JMenu("Options");
+		
+		JMenu smUpdateStore = new JMenu("Update Store");
+		smUpdateStore.add(new JMenuItem("Add Book"));
+		smUpdateStore.add(new JMenuItem("Add CV"));
+		smUpdateStore.add(new JMenuItem("Add DVD"));
+		
+		menu.add(smUpdateStore);
+		menu.add(new JMenuItem("View store"));
+		menu.add(new JMenuItem("View cart"));
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+		menuBar.add(menu);
+		
+		return menuBar;
+	}
+	
+	JPanel createHeader() {
+		JPanel header = new JPanel();
+		header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
+		
+		JLabel title = new JLabel("AIMS");
+		title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 50));
+		title.setForeground(Color.CYAN);
+		
+		JButton cart = new JButton("View cart");
+		cart.setPreferredSize(new Dimension(100, 500));
+		cart.setMaximumSize(new Dimension(100, 50));
+	}
+}
